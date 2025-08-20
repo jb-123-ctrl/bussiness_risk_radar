8b43dd419bc86b93c07b1d3f944b65ec0f6c6181
s jayabharathi <e0321048@sret.edu.in>
2025-08-20 14:23:55 +0530

    Update bussiness_risk_radar_1.py

--- bussiness_risk_radar_1.py
+++ bussiness_risk_radar_1.py
@@ -16,7 +16,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
+import streamlit as st
 df = pd.read_csv("archive (1).zip", compression="zip")
 st.write(df.head())
 df.head()
