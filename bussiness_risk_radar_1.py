4773489c6cf47da0033cbbc446128dae2c6abfb5
s jayabharathi <e0321048@sret.edu.in>
2025-08-20 14:19:38 +0530

    Update bussiness_risk_radar_1.py

--- bussiness_risk_radar_1.py
+++ bussiness_risk_radar_1.py
@@ -17,7 +17,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv("/content/archive (1).zip", compression="zip")
+df = pd.read_csv("archive (1).zip", compression="zip")
+st.write(df.head())
 df.head()
 
 print("Shape of dataset:", df.shape)
